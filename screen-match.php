20bcac8665234a1bef17e3d15be72c632e0b96b7
athena272 <guilhermera272@gmail.com>
2025-03-24 22:30:08 -0300

    Update screen-match.php

--- screen-match.php
+++ screen-match.php
@@ -3,10 +3,11 @@
 echo "Welcome to Screen Match!\n";
 
 $movieName = "Top Gun - Maverick";
-$releaseYear = 2022;
+$releaseYear = $argv[1];
 $sumMovieRating =  9 + 6 + 8 + 7.5 + 5;
 $movieRating = $sumMovieRating / 5;
 $isIncludedOnPlan = true;
 
 echo "Movie name: " . $movieName . "\n";
+echo "Release year: $releaseYear\n";
 echo "Movie rating: $movieRating";
