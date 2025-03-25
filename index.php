423e7a2e5a2e1f2e2b2d4bdf1617148901e85dff
athena272 <guilhermera272@gmail.com>
2025-03-25 17:41:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 echo "Welcome to Screen Match!\n";
 
 $movieName = "Top Gun - Maverick";
+$movieName = "Hangover";
+$movieName = "Avengers";
+
 $releaseYear = $argv[1] ?? 2022;
 $sumMovieRating =  9 + 6 + 8 + 7.5 + 5;
 $movieRating = $sumMovieRating / 5;
@@ -14,10 +17,17 @@ echo "Movie rating: $movieRating\n";
 
 //Código omitido
 if ($releaseYear > 2022) {
-    echo "Esse filme é um lançamento";
+    echo "Esse filme é um lançamento\n";
 } elseif($releaseYear > 2020 && $releaseYear <= 2022) {
-    echo "Esse filme ainda é novo";
+    echo "Esse filme ainda é novo\n";
 } else {
-    echo "Esse filme não é um lançamento";
+    echo "Esse filme não é um lançamento\n";
 }
 
+$movieGender = match ($movieName) {
+    "Top Gun - Maverick" => "action",
+    "Avengers" => "superhero",
+    "Hangover" => "comedy",
+};
+
+echo "The gender is: $movieGender";
