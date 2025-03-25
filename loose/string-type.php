7e6d9c433e84696b90ec94cc1af4c4cd0848e2d4
athena272 <guilhermera272@gmail.com>
2025-03-24 22:27:36 -0300

    Update string-type.php

--- loose/string-type.php
+++ loose/string-type.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "5" + "10";
+echo "5" + "10" . "\n";
+
+$saudacao = "Olá, meu nome é ";
+$nome = "Alice ";
+$continuacao = "e minha idade é ";
+$idade = 17;
+echo "$saudacao . $nome . $continuacao" . $idade . "\n";
+echo "$saudacao $nome $continuacao $idade";
