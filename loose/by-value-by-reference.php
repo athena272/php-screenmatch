6f90760aa1f665a14dc57f568ebb4159dc13c3c4
athena272 <guilhermera272@gmail.com>
2025-05-11 19:02:38 -0300

    Create by-value-by-reference.php

---
+++ loose/by-value-by-reference.php
@@ -0,0 +1,20 @@
+<?php
+
+function doubleByValue(float $value) : float {
+    $value *= 2;
+    return $value;
+}
+
+function doubleByReference(float &$value) : float {
+    $value *= 2;
+    return $value;
+}
+
+$originalValue = 2;
+$newValue = doubleByValue($originalValue);
+echo "Original: $originalValue\n";
+echo "New: $newValue\n";
+
+$newValue = doubleByReference($originalValue);
+echo "Original: $originalValue\n";
+echo "New: $newValue\n";
