00fda02b2b656bfbba98f743272b1064a3a78ac2
athena272 <guilhermera272@gmail.com>
2025-05-11 19:03:47 -0300

    Update screen-match.php

--- screen-match.php
+++ screen-match.php
@@ -1,20 +1,5 @@
 <?php
 
-function displayReleaseMessage(int $year): void
-{
-    if ($year > 2022) {
-        echo "This movie is a new release\n";
-    } elseif ($year > 2020 && $year <= 2022) {
-        echo "This movie is still recent\n";
-    } else {
-        echo "This movie is not a new release\n";
-    }
-}
-
-function isIncludedInPlan(bool $isPrime, int $releaseYear) : string {
-    return ($isPrime || $releaseYear < 2020) ? "Yes" : "No";
-}
-
 echo "Welcome to Screen Match!\n";
 
 $movieName = "Top Gun - Maverick";
