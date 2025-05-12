8a9c33781e0c9d72339919568f35aaefc1c63859
athena272 <guilhermera272@gmail.com>
2025-05-11 22:07:42 -0300

    Update success.php

--- public/success.php
+++ public/success.php
@@ -1,1 +1,24 @@
-<h1>Sucesso! Filme <?php echo $_GET['movie']; ?> inserido</h1>
+<?php
+$jsonFileContent = file_get_contents('movie.json');
+$movie = json_decode($jsonFileContent, true);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inserted Movie</title>
+</head>
+<body>
+<h1><?= $movie['name']; ?></h1>
+<dl>
+    <dt>Release Year</dt>
+    <dd><?= $movie['year']; ?></dd>
+
+    <dt>Rating</dt>
+    <dd><?= $movie['rating']; ?></dd>
+
+    <dt>Genre</dt>
+    <dd><?= $movie['genre']; ?></dd>
+</dl>
+</body>
+</html>
