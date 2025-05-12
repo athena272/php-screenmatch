2128b1aeeb3f2c68bb2d4a930a849d1e009f9f74
athena272 <guilhermera272@gmail.com>
2025-05-11 21:07:30 -0300

    Delete import.php

--- import.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$filePath = __DIR__ . '/movie.json';
-if (file_exists($filePath)) {
-    $fileContent = file_get_contents($filePath);
-    $movieContent = json_decode($fileContent, true);
-    var_dump($movieContent);
-}
-
