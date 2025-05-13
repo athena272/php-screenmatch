753c37657e8f8b70c5de8a1438854e8c258a569c
athena272 <guilhermera272@gmail.com>
2025-05-13 14:18:02 -0300

    Update Movie.php

--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -1,23 +1,11 @@
 <?php
 
-class Movie {
-    private array $ratings;
+class Movie extends Title {
 
-    public function __construct(public readonly string $name, public readonly int $releaseYear, public readonly Genre $genre, public readonly int $durationInMinutes)
-    {
-        $this->ratings = [];
+    public function __construct(string $name, int $releaseYear, Genre $genre, public readonly int $durationInMinutes) {
+        parent::__construct($name, $releaseYear, $genre);
     }
 
-    function rate(float $rating): void {
-        $this->ratings[] = $rating;
-    }
-
-    function average(): float {
-        $sum = array_sum($this->ratings);
-        $count = count($this->ratings);
-
-        return $sum / $count;
-    }
 }
 
 
