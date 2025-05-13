bdda88df30c03acdba9995b5ae193f9747726f91
athena272 <guilhermera272@gmail.com>
2025-05-13 09:26:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,18 @@
 <?php
 
-require "./src/functions.php";
+require __DIR__ . '/src/Model/Movie.php';
 
 echo "Welcome to Screen Match!\n";
 
-$movieName = "Top Gun - Maverick";
-$releaseYear = 2022;
+$movie = new Movie();
+$movie->name = "Thor: Ragnarok";
+$movie->year = 2021;
+$movie->genre = "superhero";
+$movie->rate(10);
+$movie->rate(10);
+$movie->rate(5);
+$movie->rate(5);
 
-$numberOfRatings = $argc - 1;
-$ratings = [];
+var_dump($movie);
 
-for ($i = 1; $i < $argc; $i++) {
-    $ratings[] = (float) $argv[$i];
-}
-
-$movieRating = array_sum($ratings) / $numberOfRatings;
-$includedInPlan = isIncludedInPlan(true, $releaseYear);
-
-echo "Movie name: " . $movieName . "\n";
-echo "Movie rating: $movieRating\n";
-echo "Release year: $releaseYear\n";
-
-$genre = match ($movieName) {
-    "Top Gun - Maverick" => "action",
-    "Thor: Ragnarok" => "superhero",
-    "The Hangover" => "comedy",
-    default => "unknown genre",
-};
-
-echo "The movie genre is: $genre\n";
-
-$movie = createMovie(name: "Thor: Ragnarok", year: 2021, rating: 7.8, genre: "superhero");
-
-echo "{$movie->name}\n";
-
-$movieJSON = json_encode($movie);
-file_put_contents(__DIR__ . '/src/movie.json', $movieJSON);
+echo $movie->average();
