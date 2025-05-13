0e34a29b003b9d986845bb28774bea8faa509c2f
athena272 <guilhermera272@gmail.com>
2025-05-13 09:26:42 -0300

    Update Movie.php

--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -2,8 +2,21 @@
 
 class Movie {
     public string $name;
-    public int $year;
-    public float $rating;
+    public int $releaseYear;
     public string $genre;
+    public array $ratings = [];
+
+    function rate(float $rating): void {
+        $this->ratings[] = $rating;
+    }
+
+    function average(): float {
+        $sum = array_sum($this->ratings);
+        $count = count($this->ratings);
+
+        return $sum / $count;
+    }
 }
+
+
 
