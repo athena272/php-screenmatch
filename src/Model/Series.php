db43ba30e8c0e114a9e864d7a23c2bb65a83e86b
athena272 <guilhermera272@gmail.com>
2025-05-13 15:40:19 -0300

    Update Series.php

--- src/Model/Series.php
+++ src/Model/Series.php
@@ -4,15 +4,7 @@ class Series extends Title
 {
     private array $ratings;
 
-    public function __construct(
-        public readonly string $name,
-        public readonly int    $releaseYear,
-        public readonly Genre  $genre,
-        public readonly int $seasons,
-        public readonly int $episodesPerSeason,
-        public readonly int $minutesPerEpisode,
-    )
-    {
+    public function __construct(string $name, int    $releaseYear, Genre  $genre, public readonly int $seasons, public readonly int $episodesPerSeason, public readonly int $minutesPerEpisode) {
         parent::__construct($name, $releaseYear, $genre);
     }
 }
