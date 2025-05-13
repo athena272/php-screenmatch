c94e7d863ae654b4e3c2e67e931390fb98996799
athena272 <guilhermera272@gmail.com>
2025-05-13 11:06:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ require __DIR__ . '/src/Model/Movie.php';
 
 echo "Welcome to Screen Match!\n";
 
-$movie = new Movie();
-$movie->setName("Thor: Ragnarok");
-$movie->setReleaseYear(2021);
-$movie->setGenre("superhero");
+$movie = new Movie("Thor: Ragnarok", 2021, "superhero");
 $movie->rate(10);
 $movie->rate(10);
 $movie->rate(5);
