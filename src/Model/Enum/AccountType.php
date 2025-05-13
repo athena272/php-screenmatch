e1c9337f55b2156259ec24e131afe0f3aaab2ea1
athena272 <guilhermera272@gmail.com>
2025-05-13 16:05:51 -0300

    Create AccountType.php

---
+++ src/Model/Enum/AccountType.php
@@ -0,0 +1,20 @@
+<?php
+
+enum AccountType
+{
+    case Checking;
+    case Savings;
+    case Student;
+    case Investment;
+
+    public function hasFee(): bool
+    {
+        return match ($this) {
+            AccountType::Checking,
+            AccountType::Investment => true,
+
+            AccountType::Savings,
+            AccountType::Student => false,
+        };
+    }
+}
