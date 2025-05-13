b60c7456ce4d91b20f818c4fcef7153c8b18a635
athena272 <guilhermera272@gmail.com>
2025-05-13 09:35:17 -0300

    Update Movie.php

--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -1,10 +1,10 @@
 <?php
 
 class Movie {
-    public string $name;
-    public int $releaseYear;
-    public string $genre;
-    public array $ratings = [];
+    private string $name;
+    private int $releaseYear;
+    private string $genre;
+    private array $ratings = [];
 
     function rate(float $rating): void {
         $this->ratings[] = $rating;
@@ -15,6 +15,33 @@ class Movie {
         $count = count($this->ratings);
 
         return $sum / $count;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getReleaseYear(): int
+    {
+        return $this->releaseYear;
+    }
+
+    public function setReleaseYear(int $releaseYear): void {
+        $this->releaseYear = $releaseYear;
+    }
+
+    public function getGenre(): string {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): void {
+        $this->genre = $genre;
     }
 }
 
