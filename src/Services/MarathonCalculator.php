d669712b9a2f9cd5c13de72d9555dd01ecf65baf
athena272 <guilhermera272@gmail.com>
2025-05-13 21:17:49 -0300

    Create MarathonCalculator.php

---
+++ src/Services/MarathonCalculator.php
@@ -0,0 +1,20 @@
+<?php
+class MarathonCalculator
+{
+    private int $totalDuration = 0;
+
+    public function include(Title $title): void
+    {
+        if ($title instanceof Movie) {
+            $this->totalDuration += $title->durationInMinutes;
+        } elseif ($title instanceof Series) {
+            $this->totalDuration +=
+                $title->seasons * $title->episodesPerSeason * $title->minutesPerEpisode;
+        }
+    }
+
+    public function getTotalDuration(): int
+    {
+        return $this->totalDuration;
+    }
+}
