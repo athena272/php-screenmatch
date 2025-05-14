8fffa91ea02a225f17a3e5b45efec38ecf7d0cde
athena272 <guilhermera272@gmail.com>
2025-05-13 21:31:15 -0300

    Update Movie.php

--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -3,11 +3,21 @@
 class Movie extends Title
 {
 
-    public function __construct(string $name, int $releaseYear, Genre $genre, public readonly int $durationInMinutes)
+    public function __construct(
+        string              $name,
+        int                 $releaseYear,
+        Genre               $genre,
+        public readonly int $durationInMinutes
+    )
     {
         parent::__construct($name, $releaseYear, $genre);
     }
 
+    #[\Override]
+    public function durationInMinutes(): int
+    {
+        return $this->durationInMinutes;
+    }
 }
 
 
