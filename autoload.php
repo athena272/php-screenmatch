433d0eb59c125628f21d401f23674c9537fe355c
athena272 <guilhermera272@gmail.com>
2025-05-14 21:56:20 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -4,5 +4,8 @@ spl_autoload_register(function (string $className) {
     $path = str_replace('ScreenMatch', 'src', $className) . '.php';
     $path = str_replace("\\", DIRECTORY_SEPARATOR, $path);
 
-    require_once __DIR__ . DIRECTORY_SEPARATOR . $path;
+    $fullPath = __DIR__ . DIRECTORY_SEPARATOR . $path;
+    if (file_exists($fullPath)) {
+        require_once $fullPath;
+    }
 });
