5c233238f38a4596016419aa0456a3cf2749c989
athena272 <guilhermera272@gmail.com>
2025-05-15 18:50:31 -0300

    Update WithRating.php

--- src/Traits/WithRating.php
+++ src/Traits/WithRating.php
@@ -2,12 +2,21 @@
 
 namespace Screenmatch\Traits;
 
+use InvalidArgumentException;
+
 trait WithRating
 {
     private array $ratings = [];
 
+    /**
+     * @throws InvalidArgumentException when score is negative or bigger than 10
+     */
     public function rate(float $score): void
     {
+        if ($score < 0 || $score > 10) {
+            throw new InvalidArgumentException('Score must be between 0 and 10');
+        }
+
         $this->ratings[] = $score;
     }
 
